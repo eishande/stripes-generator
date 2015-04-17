7c313a0d76129dcde57a7c80102483e8c2524d9b
Kate Buckner <ingidora@gmail.com>
2015-04-17 16:44:48 -0400

    Fixed database migration problem w/ patterns table, renamed database

--- db/migrate/20150413161329_add_columns_to_patterns.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToPatterns < ActiveRecord::Migration
-  def change
-    add_column :patterns, :colors, :string, array: true, default: []
-    add_column :patterns, :user_id, :integer, null: false
-    add_column :patterns, :dataset_id, :integer, null: false
-  end
-end
