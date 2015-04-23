c49000d64f37330e007aabe209bb22fc4fa5b1db
Kate Buckner <ingidora@gmail.com>
2015-04-23 15:23:49 -0400

    Further front-end styling - index page views, forms

--- app/assets/javascripts/color_picker.js
+++ app/assets/javascripts/color_picker.js
@@ -1,5 +1,3 @@
-$('.flash-messages').delay(1500).fadeOut(600);
-
 $('#color1').minicolors().minicolors('value','#fef0d9');
 $('#color2').minicolors().minicolors('value','#fdcc8a');
 $('#color3').minicolors().minicolors('value','#fc8d59');
