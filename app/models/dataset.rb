6775a6e0ab726cb92a14d4ae244edda40da3a9eb
Kate Buckner <ingidora@gmail.com>
2015-04-23 15:23:06 -0400

    Added a validation for length of dataset

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -6,6 +6,7 @@ class Dataset < ActiveRecord::Base
   validates :data, presence: true
 
   validate :numericality_of_data
+  validate :dataset_length
 
   def numericality_of_data
     self.data.each do |value|
@@ -13,6 +14,12 @@ class Dataset < ActiveRecord::Base
         errors.add(:data, "Data must be numeric")
         break
       end
+    end
+  end
+
+  def dataset_length
+    if data.length < 5
+      errors.add(:data, "Dataset must include at least 5 values")
     end
   end
 end
