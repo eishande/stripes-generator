a354f81ed557087de4e27ff9e864247ce553fe79
Kate Buckner <ingidora@gmail.com>
2015-04-23 10:20:11 -0400

    narrower color bars to accommodate smaller screen, first steps to make color picker render on editing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,15 +16,8 @@
 //= require foundation
 //= require slick
 //= require slider
+//= require color_picker
 
 $(function(){ $(document).foundation(); });
 
-$(function() {
-  $('.flash-messages').delay(1500).fadeOut(600);
-
-  $('#color1').minicolors().minicolors('value', '#fef0d9');
-  $('#color2').minicolors().minicolors('value','#fdcc8a');
-  $('#color3').minicolors().minicolors('value','#fc8d59');
-  $('#color4').minicolors().minicolors('value','#e34a33');
-  $('#color5').minicolors().minicolors('value','#b30000');
-});
+$('.flash-messages').delay(1500).fadeOut(600);
---
+++ app/assets/javascripts/color_picker_edit.js
@@ -0,0 +1,7 @@
+//value needs to be able to take the color values from the db or params
+
+$('#color1').minicolors().minicolors('value','#fef0d9');
+$('#color2').minicolors().minicolors('value','#fdcc8a');
+$('#color3').minicolors().minicolors('value','#fc8d59');
+$('#color4').minicolors().minicolors('value','#e34a33');
+$('#color5').minicolors().minicolors('value','#b30000');
